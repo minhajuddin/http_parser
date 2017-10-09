1668c9fc601f65ea870d96c697cc972c5ac234cf
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-10-09 15:42:34 +0530

    added initial request creator for get requests

--- lib/http_parser.ex
+++ lib/http_parser.ex
@@ -1,18 +1,30 @@
 defmodule HttpParser do
-  @moduledoc """
-  Documentation for HttpParser.
-  """
+  @line_break "\r\n"
+  @space " "
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HttpParser.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defmodule Request do
+    defstruct uri: %URI{},
+      method: :get,
+      http_version: :http1_1,
+      headers: []
   end
+
+  def create_request(%Request{} = rq) do
+    [
+      "GET ", path(rq.uri), @space, http_version_text(rq.http_version),
+      @line_break,
+      "Host: ", rq.uri.host,
+      @line_break,
+      rq.headers |> Enum.map(&render_header/1), # ends with a line_break
+    ]
+    |> IO.iodata_to_binary
+  end
+
+  defp render_header({name, value}), do: [name, ?:, @space, value, @line_break]
+
+  defp http_version_text(:http1_1), do: "HTTP/1.1"
+  defp http_version_text(:http1_0), do: "HTTP/1.0"
+
+  defp path(%URI{path: path, query: nil}), do: path
+  defp path(%URI{path: path, query: q}), do: [path, ??, q]
 end
--- test/http_parser_test.exs
+++ test/http_parser_test.exs
@@ -2,7 +2,65 @@ defmodule HttpParserTest do
   use ExUnit.Case
   doctest HttpParser
 
-  test "greets the world" do
-    assert HttpParser.hello() == :world
+  alias HttpParser.Request
+
+  def req(path \\ "/foobar"), do: %Request{uri: URI.parse("https://localhost:4000#{path}")}
+
+  describe "GET create_request" do
+    import HttpParser, only: [create_request: 1]
+
+    test "uses the right verb" do
+      assert "GET " <> _ = req() |> create_request
+    end
+
+    test "sets the path" do
+      assert "GET /foobar HTTP" <> _ = req() |> create_request
+    end
+
+    test "sets the query string" do
+      assert "GET /fooey?awesome=bar HTTP" <> _ = req("/fooey?awesome=bar#test") |> create_request
+    end
+
+    test "sets the host" do
+      assert req() |> create_request =~ ~r[^GET /foobar HTTP/1.1\r\nHost: localhost]
+    end
+
+    test "uses CRLF line endings in header" do
+      refute req() |> create_request |> String.replace("\r\n", "") |> String.contains?("\n")
+    end
+
+    test "appends headers" do
+      r = req()
+      r = %{r | headers: [{"User-Agent", "danny"}, {"Accept", "html"}]}
+      assert r |> create_request =~ ~r[\r\nUser-Agent: danny\r\nAccept: html\r\n]
+    end
+
+    test "uses valid http protocol string" do
+      r = req()
+
+      r = %{r | http_version: :http1_1}
+      assert r |> create_request |> String.contains?("HTTP/1.1")
+
+      r = %{r | http_version: :http1_0}
+      assert r |> create_request |> String.contains?("HTTP/1.0")
+    end
+
+    test "creates a complex request" do
+      r = %Request{
+        uri: URI.parse("https://dannyisawesome.com/romeo-AND-juliet?by=dire-straits#juliet-juliet"),
+        headers: [
+          {"Content/Type", "application/json"},
+          {"Accept", "application/json"},
+          {"User-Agent", "dann"},
+        ]
+      }
+      assert r |> create_request == "\
+GET /romeo-AND-juliet?by=dire-straits HTTP/1.1\r\n\
+Host: dannyisawesome.com\r\n\
+Content/Type: application/json\r\n\
+Accept: application/json\r\n\
+User-Agent: dann\r\n"
+    end
+
   end
 end
