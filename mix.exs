270542993146afb9ae8b0599488e2c489c44d1c3
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-10-09 17:21:14 +0530

    published to hex.pm

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule HttpParser.Mixfile do
   def project do
     [
       app: :http_parser,
+      description: "A complete HTTP parser written in pure Elixir",
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
     ]
   end
 
@@ -21,8 +23,16 @@ defmodule HttpParser.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/minhajuddin/http_parser"},
+      maintainers: ["Khaja Minhajuddin"],
     ]
   end
 end
